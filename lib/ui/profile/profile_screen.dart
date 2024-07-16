dd0cd389387aed009a1c06843f1893d7da28fe3e
mustafatihgunduz <mustafatihgunduz@gmail.com>
2024-07-16 21:53:30 +0300

    Profil Sayfası Tasarım Ilerlendi

--- lib/ui/profile/profile_screen.dart
+++ lib/ui/profile/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notumcepte/ui/profile/utils/profile_card.dart';
+import 'package:notumcepte/utility/size_config.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -11,11 +13,88 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Profil",
+      backgroundColor: Colors.grey[900],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Column(
+              // ? Avatar ve İsim için oluşturulan column
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: SizeConfig.screenHeight! * 0.04,
+                  ),
+                  child: Container(
+                    height: SizeConfig.screenHeight! * 0.2,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.transparent,
+                      image: DecorationImage(
+                        image: AssetImage("assets/profile-avatar.png"),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight! * 0.03,
+                ),
+                Text(
+                  "Mustafa Fatih GÜNDÜZ",
+                  style: TextStyle(
+                      fontSize: SizeConfig.fontSize!, color: Colors.white),
+                )
+              ],
+            ),
+            SizedBox(
+              height: SizeConfig.screenHeight! * 0.04,
+            ),
+            CustomProfileCard(
+              title: 'Kullanıcı Bilgilerimi Güncelle',
+              icon: Icons.person_search_rounded,
+              style: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+            CustomProfileCard(
+              title: 'Eğitim Bilgilerimi Güncelle',
+              icon: Icons.school,
+              style: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+            CustomProfileCard(
+              title: 'Banka Bilgilerimi Güncelle',
+              icon: Icons.credit_card,
+              style: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+            CustomProfileCard(
+              title: 'Satın Alma Geçmişi',
+              icon: Icons.history,
+              style: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+            CustomProfileCard(
+              title: 'Gizlilik Politikası',
+              icon: Icons.privacy_tip,
+              style: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+          ],
         ),
       ),
+      floatingActionButton: CustomProfileCard(
+        title: 'Çıkış Yap',
+        icon: Icons.logout,
+        trailingColor: Colors.red,
+        leadingColor: Colors.red,
+        style: TextStyle(color: Colors.red),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
---
+++ lib/ui/profile/utils/profile_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:notumcepte/utility/size_config.dart';
+
+class CustomProfileCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final TextStyle? style;
+  Color trailingColor;
+  Color leadingColor;
+
+  CustomProfileCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    this.style,
+    this.trailingColor = Colors.white,
+    this.leadingColor = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: SizeConfig.screenWidth! * 0.03,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(4, 4),
+              blurRadius: 15,
+              spreadRadius: 1,
+              color: Colors.black,
+            ),
+            BoxShadow(
+              offset: Offset(-2, -2),
+              blurRadius: 15,
+              spreadRadius: 0.1,
+              color: Colors.grey.shade800,
+            ),
+          ],
+        ),
+        child: Card(
+            color: Colors.grey.shade900,
+            child: ListTile(
+              title: Text(
+                title,
+                style: style,
+              ),
+              leading: Icon(
+                icon,
+                color: leadingColor,
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: SizeConfig.screenWidth! * 0.04,
+                color: trailingColor,
+              ),
+            )),
+      ),
+    );
+  }
+}
